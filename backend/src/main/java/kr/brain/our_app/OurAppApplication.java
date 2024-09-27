bfe06918f342f34f5f77155c0f4cb35759e95118
junyonglee0223 <ljyong3339@gmail.com>
2024-09-27 11:02:15 +0900

    spring initializr

---
+++ backend/src/main/java/kr/brain/our_app/OurAppApplication.java
@@ -0,0 +1,23 @@
+package kr.brain.our_app;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class OurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurAppApplication.class, args);
+	}
+
+	@RestController
+	public static class HelloController{
+		@GetMapping("/")
+		public String Hello(){
+			return "Hello World!";
+		}
+	}
+}
---
+++ backend/src/test/java/kr/brain/our_app/OurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.brain.our_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
