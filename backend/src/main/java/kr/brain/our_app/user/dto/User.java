8ac7e0bb9fd84852a3fa96db65049c4225891694
junyonglee0223 <ljyong3339@gmail.com>
2024-09-29 00:03:12 +0900

    user mvc and connect to h2

---
+++ backend/src/main/java/kr/brain/our_app/user/dto/User.java
@@ -0,0 +1,18 @@
+package kr.brain.our_app.user.dto;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+}
