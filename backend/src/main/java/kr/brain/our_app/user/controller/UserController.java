e382d0a449b5adec56596a94150e45f4e4717428
junyonglee0223 <ljyong3339@gmail.com>
2024-10-04 12:14:56 +0900

    set html view and pageController

--- backend/src/main/java/kr/brain/our_app/user/controller/UserController.java
+++ backend/src/main/java/kr/brain/our_app/user/controller/UserController.java
@@ -3,9 +3,11 @@ package kr.brain.our_app.user.controller;
 import kr.brain.our_app.user.dto.User;
 import kr.brain.our_app.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,7 +25,14 @@ public class UserController {
         return ResponseEntity.ok(createdUser);
     }
 
-    //id를 url에 노출시키는 것 방지해야 함 -- 수정 필수
+    // 이메일로 사용자 정보 조회
+    @GetMapping("/profile")
+    public ResponseEntity<User> findUserByEmail(@RequestParam String email) {
+        Optional<User> user = userService.findByEmail(email);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(404).build());
+    }
+
+    //id를 url에 노출시키는 것 방지해야 함 -- 수정 필수 -> /profile, requestParam 사용
     @GetMapping("/{id}")
     public ResponseEntity<User> findUser(@PathVariable Long id){
         return userService.findById(id)
--- backend/src/main/java/kr/brain/our_app/user/dto/User.java
+++ backend/src/main/java/kr/brain/our_app/user/dto/User.java
@@ -1,13 +1,17 @@
 package kr.brain.our_app.user.dto;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
