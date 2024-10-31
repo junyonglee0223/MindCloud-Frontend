376d698db43d332db4d439c5f71b9491168898c1
junyonglee0223 <ljyong3339@gmail.com>
2024-10-31 15:29:29 +0900

    user repo, service, control refactoring

--- backend/src/main/java/kr/brain/our_app/user/service/UserService.java
+++ backend/src/main/java/kr/brain/our_app/user/service/UserService.java
@@ -1,12 +1,15 @@
 package kr.brain.our_app.user.service;
 
+import kr.brain.our_app.idsha.IDGenerator;
 import kr.brain.our_app.user.domain.User;
 import kr.brain.our_app.user.dto.UserDto;
 import kr.brain.our_app.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -15,8 +18,23 @@ public class UserService {
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
-    public User createUser(User user){
-        return userRepository.save(user);
+    public UserDto createUser(UserDto userDto){
+        if(userRepository.existsByEmail(userDto.getEmail())){
+            throw new IllegalArgumentException("User with this email already exists.");
+        }
+
+        String createId = IDGenerator.generateId(userDto.getEmail());
+        User user = User.builder()
+                .id(createId)
+                .userName(userDto.getUserName())
+                .email(userDto.getEmail())
+                .build();
+        User createdUser = userRepository.save(user);
+        return UserDto.builder()
+                .id(createdUser.getId())
+                .userName(createdUser.getUserName())
+                .email(createdUser.getEmail())
+                .build();
     }
     public Optional<User> findById(String id){
         return userRepository.findById(id);
@@ -33,7 +51,17 @@ public class UserService {
                 .build();
     }
     /**************************************************/
-    public List<User> findAll(){
-        return userRepository.findAll();
+    public List<UserDto> findAll(){
+        List<User> userList = userRepository.findAll();
+        List<UserDto>userDtoList = userList.stream()
+                .map(
+                        user -> UserDto.builder()
+                                .id(user.getId())
+                                .userName(user.getUserName())
+                                .email(user.getEmail())
+                                .build()
+                ).collect(Collectors.toList());
+
+        return userDtoList;
     }
 }
