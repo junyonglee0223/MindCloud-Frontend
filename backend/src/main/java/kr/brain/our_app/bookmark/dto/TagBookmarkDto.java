15c3d68788abed39e689d2a856422df5512fb0db
junyonglee0223 <ljyong3339@gmail.com>
2024-11-05 15:58:38 +0900

    fix: tag, bookmark name -> id로 변경

--- backend/src/main/java/kr/brain/our_app/bookmark/dto/TagBookmarkDto.java
+++ backend/src/main/java/kr/brain/our_app/bookmark/dto/TagBookmarkDto.java
@@ -8,6 +8,6 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TagBookmarkDto {
-    private String tagName;
-    private String bookmarkName;
+    private String tagId;
+    private String bookmarkId;
 }
